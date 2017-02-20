7531f2be52ec61a076a4d9ba9272c20231732d75
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-20 16:43:20 -0500

    Fade out sign-in window when close button is clicked

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
   $('.signin').on('click', function() {
     $('.modal').fadeIn();
   });
+
+  $('.close').on('click', function() {
+    $('.modal').fadeOut();
+  });
+
 });
